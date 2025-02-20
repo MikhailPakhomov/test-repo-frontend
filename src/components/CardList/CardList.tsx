919ebe1474487a98f1825bcb37605c9a20414973
Михаил Пахомов <mihail93@mail.ru>
2025-02-20 22:27:21 +0300

    feat: add edit input field

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,25 +1,28 @@
-import CardListItem from './CardListItem/CardListItem';
-import { TCardItem } from '../../types/CardItem';
-import { For, Match, Switch } from 'solid-js';
-import { fetchUsers } from './../../api/apiClient';
-import { createQuery } from '@tanstack/solid-query';
+import CardListItem from "./CardListItem/CardListItem";
+import { TCardItem } from "../../types/CardItem";
+import { For, Match, Switch } from "solid-js";
+import { fetchUsers } from "./../../api/apiClient";
+import { createQuery } from "@tanstack/solid-query";
 const CardList = () => {
-  const users = createQuery(() => ['users'], fetchUsers);
+  const users = createQuery(() => ["users"], fetchUsers);
 
   return (
     <>
       <h1>Users</h1>
-      <div class="grid grid-cols-4 gap-4">
-        <Switch>
-          <Match when={users.isLoading}>Loading...</Match>
-          <Match when={users.isError}>
-            <span>Error: {(users.error as Error).message}</span>
-          </Match>
-          <Match when={users.data !== undefined}>
+
+      <Switch>
+        <Match when={users.isLoading}>
+          <span class="loading loading-dots loading-lg"></span>
+        </Match>
+        <Match when={users.isError}>
+          <span>Error: {(users.error as Error).message}</span>
+        </Match>
+        <Match when={users.data !== undefined}>
+          <div class="grid grid-cols-4 gap-4">
             <For each={users.data}>{(user) => <CardListItem {...user} />}</For>
-          </Match>
-        </Switch>
-      </div>
+          </div>
+        </Match>
+      </Switch>
     </>
   );
 };
